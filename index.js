e7767b6eb6ed4f0cb6b41f4ab1644575d59e9660
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-07-06 15:08:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 function deleteFromObjectByKey(object, key)
 {
 
-var newObject = Object.assign({}, object);
 
-delete newObject.key;
-return newObject;
+
+delete object.key;
+return object;
  
 
 
